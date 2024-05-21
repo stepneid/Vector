eee0197c0f586c0b6ebb1675b01964ef8e0f696e
stepneid <167896451+stepneid@users.noreply.github.com>
2024-05-21 16:14:19 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector> 
+#include <cmath>
+using namespace std;
+int main()
+{
+	vector<int> v{ 1, 2, 3, 4 };
+	v += 10;
+	for (const int ele : v) {
+		cout << ele << " ";
+		cout << "\n";
+	}
+	v /= 2;
+	for (const int ele : v) {
+		cout << ele << " ";
+	}
+	return 0;
+}
